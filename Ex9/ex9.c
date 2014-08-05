6acabe08eb62a821fa1d77fb946634224d68f746
Frederick-S <mao_xiaodan@hotmail.com>
2014-08-05 15:27:04 +0800

    Update ex9.c

--- Ex9/ex9.c
+++ Ex9/ex9.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     int numbers[4] = {0};
-    char name [4] = {'a'};
+    char name[4] = {'a'};
 
     // first, print them out raw
     printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
