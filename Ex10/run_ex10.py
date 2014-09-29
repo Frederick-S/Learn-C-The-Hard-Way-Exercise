e4cb960be3dc83eade3b2aae14788b57833319f8
Frederick-S <mao_xiaodan@hotmail.com>
2014-09-29 05:18:49 +0000

    Update python script

--- Ex10/run_ex10.py
+++ Ex10/run_ex10.py
@@ -8,6 +8,8 @@ if len(sys.argv) > 1:
         times = int(sys.argv[1])
         arguments = [str(i) for i in range(times)]
         arguments.insert(0, "./ex10")
+
+        # Run ex10
         call(arguments)
     except Exception, e:
         print e
