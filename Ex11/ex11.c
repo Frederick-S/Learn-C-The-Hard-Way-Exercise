31a550602c472718136146a405880ce896213f7a
Frederick-S <mao_xiaodan@hotmail.com>
2014-10-20 08:27:11 +0000

    Add ex11.c

---
+++ Ex11/ex11.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // go through each string in argv
+
+    int i = 0;
+    while (i < argc) {
+        printf("arg %d: %s\n", i, argv[i]);
+        i++;
+    }
+
+    // let's make our own array of strings
+    char *states[] = {
+        "California", "Oregon",
+        "Washington", "Texas"
+    };
+
+    int num_states = 4;
+
+    // watch for this
+    i = 0; 
+    while (i < num_states) {
+        printf("state %d: %s\n", i, states[i]);
+        i++;
+    }
+
+    return 0;
+}
