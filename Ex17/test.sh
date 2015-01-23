5f04ff21ce7753d0fa828ba882350e431c6af810
Frederick-S <mao_xiaodan@hotmail.com>
2015-01-23 08:13:46 +0000

    Add test

---
+++ Ex17/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Creating database ..."
+./ex17 db.dat c
+echo ""
+
+echo "Saving records to database ..."
+./ex17 db.dat s 1 zed zed@zedshaw.com
+./ex17 db.dat s 2 frank frank@zedshaw.com
+./ex17 db.dat s 3 joe joe@zedshaw.com
+echo ""
+
+echo "Printing all records ..."
+./ex17 db.dat l
+echo ""
+
+echo "Deleting record by id 3 ..."
+./ex17 db.dat d 3
+echo ""
+
+echo "Printing all records ..."
+./ex17 db.dat l
+echo ""
+
+echo "Getting record by id 2 ..."
+./ex17 db.dat g 2
